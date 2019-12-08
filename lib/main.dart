6bd0ea023b7d910ff3e51456b951211ff921c735
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-12-08 13:13:43 +0000

    Basic Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(new HelloWorldApp());
 
+class HelloWorldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Material(
+        child: new Center(
+          child: new Text("Hello world!"),
+        ),
+      ),
+    );
+  }
+}
