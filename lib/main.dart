30d597db1abc15ac32ed30276d60eb6d3c252a19
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-12-08 20:52:54 +0000

    Add blinking point and apply layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,35 @@ class OminousBeepingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        TextSection('Ominous'),
-        TextSection('Beeping'),
-        TextSection('App'),
-      ],
-    ),
-    );
-    new BlinkingPoint(
-      xCoor: 100.0, // The x coordinate of the point
-      yCoor: 500.0, // The y coordinate of the point
-      pointColor: Colors.red, // The color of the point
-      pointSize: 10.0, // The size of the point
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          new Container(
+            child: Align(
+              alignment: Alignment.center,
+              child: BlinkingPoint(
+                xCoor: 1.0, // The x coordinate of the point
+                yCoor: 1.0, // The y coordinate of the point
+                pointColor: Colors.red, // The color of the point
+                pointSize: 10.0, // The size of the point
+              ),
+            ),
+          ),
+          new Container(padding: EdgeInsets.symmetric(vertical: 150.0)),
+          new Container(
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Column(
+                children: <Widget>[
+                  TextSection('Ominous'),
+                  TextSection('Beeping'),
+                  TextSection('App'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/text_section.dart
+++ lib/text_section.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 class TextSection extends StatelessWidget {
   final String text;
-
   const TextSection(this.text);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
         child: Align(
-        alignment: Alignment.bottomLeft,
-        child: Text(text, textDirection: TextDirection.ltr)
+          alignment: Alignment.bottomLeft,
+          child: Text(text, textDirection: TextDirection.ltr)
         ),
     );
   }
